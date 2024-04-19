579b56675543855ad67b9c44940786dac6aaeadd
rass1010 <rasmusberthag@gmail.com>
2024-04-19 18:24:52 +0200

    Add project files.

---
+++ RassModMenu/Mods/AirGrab.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Photon.Pun;
+
+namespace RassMobile.Mods
+{
+    internal class AirGrab : ModFramework
+    {
+
+        bool holdR;
+        bool holdL;
+        GameObject holdableR = new GameObject();
+        GameObject holdableL = new GameObject();
+
+        public AirGrab(string name, string description) : base(name, description)
+        {
+        }
+        public override void Update()
+        {
+            base.Update();
+
+            if(ControllerInputPoller.instance.rightControllerGripFloat > 0.8 && !holdR)
+            {
+                holdR = true;
+                holdableR = new GameObject();
+                holdableR.transform.position = GorillaLocomotion.Player.Instance.rightControllerTransform.position;
+                GorillaLocomotion.Climbing.GorillaClimbable gorillaClimbable = holdableR.AddComponent<GorillaLocomotion.Climbing.GorillaClimbable>();
+                GorillaLocomotion.Climbing.GorillaHandClimber gorillaHandClimber = GorillaLocomotion.Player.Instance.rightControllerTransform.Find("GorillaHandClimber").GetComponent<GorillaLocomotion.Climbing.GorillaHandClimber>();
+                //holdable.AddComponent<PhotonView>();
+                GorillaLocomotion.Player.Instance.BeginClimbing(gorillaClimbable, gorillaHandClimber);
+            }
+            else if (ControllerInputPoller.instance.rightControllerGripFloat <= 0.8 && holdR)
+            {
+                holdR = false;
+            }
+
+            if (ControllerInputPoller.instance.leftControllerGripFloat > 0.8 && !holdL)
+            {
+                holdL = true;
+                holdableL = new GameObject();
+                holdableL.transform.position = GorillaLocomotion.Player.Instance.leftControllerTransform.position;
+                GorillaLocomotion.Climbing.GorillaClimbable gorillaClimbable = holdableL.AddComponent<GorillaLocomotion.Climbing.GorillaClimbable>();
+                GorillaLocomotion.Climbing.GorillaHandClimber gorillaHandClimber = GorillaLocomotion.Player.Instance.leftControllerTransform.Find("GorillaHandClimber").GetComponent<GorillaLocomotion.Climbing.GorillaHandClimber>();
+                //holdable.AddComponent<PhotonView>();
+                GorillaLocomotion.Player.Instance.BeginClimbing(gorillaClimbable, gorillaHandClimber);
+            }
+            else if (ControllerInputPoller.instance.leftControllerGripFloat <= 0.8 && holdL)
+            {
+                holdL = false;
+            }
+
+        }
+    }
+}
---
+++ RassModMenu/Mods/BounceMonke.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RassMobile.Mods
+{
+    internal class BounceMonke : ModFramework
+    {
+        float bounce;
+        PhysicMaterialCombine ogCombine;
+
+        public BounceMonke(string name, string description) : base(name, description)
+        {
+        }
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+            bounce = GorillaLocomotion.Player.Instance.bodyCollider.material.bounciness;
+            ogCombine = GorillaLocomotion.Player.Instance.bodyCollider.material.bounceCombine;
+            GorillaLocomotion.Player.Instance.bodyCollider.material.bounceCombine = PhysicMaterialCombine.Maximum;
+            GorillaLocomotion.Player.Instance.bodyCollider.material.bounciness = 1.0f;
+            
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+            GorillaLocomotion.Player.Instance.bodyCollider.material.bounciness = bounce;
+            GorillaLocomotion.Player.Instance.bodyCollider.material.bounceCombine = ogCombine;
+        }
+
+    }
+}
