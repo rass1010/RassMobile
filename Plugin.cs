6abb1a56b0343661c8c96ad8f36aed2507c7d530
NotMageLock <167706567+NotMageLock@users.noreply.github.com>
2024-04-22 18:43:31 -0400

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -51,6 +51,7 @@ namespace RassMobile
             mods.Add(new Upsidedown("Upsidedown", "turns you upsidedown"));
             mods.Add(new Flight("Flight", "hold trigger to fly"));
             mods.Add(new Geppo("Geppo", "hold trigger to and move your controller to launch your self (Mod idea by my brother)"));
+            mods.Add(new FirstPerson("First Person Camera", "Makes your computer have a first person view"));
             
             Utilla.Events.GameInitialized += GameInitialized;
         }
