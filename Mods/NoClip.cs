dae00a434b4fa37a78247cbf594ec9dec9b41272
rass1010 <62247959+rass1010@users.noreply.github.com>
2024-04-23 19:40:44 +0200

    Add files via upload

---
+++ Mods/NoClip.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RassMobile.Mods
+{
+    internal class NoClip : ModFramework
+    {
+
+        public static int layer = 29, layerMask = 1 << layer;
+        private LayerMask baseMask;
+
+        public NoClip(string name, string description) : base(name, description)
+        {
+        }
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+            baseMask = GorillaLocomotion.Player.Instance.locomotionEnabledLayers;
+            GorillaLocomotion.Player.Instance.locomotionEnabledLayers = layerMask;
+            GorillaLocomotion.Player.Instance.bodyCollider.isTrigger = true;
+            GorillaLocomotion.Player.Instance.headCollider.isTrigger = true;
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+            GorillaLocomotion.Player.Instance.locomotionEnabledLayers = baseMask;
+            GorillaLocomotion.Player.Instance.bodyCollider.isTrigger = false;
+            GorillaLocomotion.Player.Instance.headCollider.isTrigger = false;
+        }
+
+    }
+}
---
+++ Mods/WallRunAssist.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RassMobile.Mods
+{
+    internal class WallRunAssist : ModFramework
+    {
+        RaycastHit hit;
+        Vector3 ogGravity;
+        public WallRunAssist(string name, string description) : base(name, description)
+        {
+            ogGravity = Physics.gravity;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            if (GorillaLocomotion.Player.Instance.wasLeftHandTouching || GorillaLocomotion.Player.Instance.wasRightHandTouching)
+            {
+                FieldInfo fieldInfo = typeof(GorillaLocomotion.Player).GetField("lastHitInfoHand", BindingFlags.NonPublic | BindingFlags.Instance);
+                hit = (RaycastHit)fieldInfo.GetValue(GorillaLocomotion.Player.Instance);
+                Physics.gravity = hit.normal * -ogGravity.magnitude;
+            }
+            else
+            {
+                if (Vector3.Distance(GorillaLocomotion.Player.Instance.bodyCollider.transform.position, hit.point) > 2)
+                {
+                    Physics.gravity = ogGravity;
+                }
+            }
+
+
+
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+            Physics.gravity = ogGravity;
+        }
+
+    }
+}
